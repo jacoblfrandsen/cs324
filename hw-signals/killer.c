623c78052aa49726f50fcdb94c8904875c242acc
Jacob Lynn Frandsen <jfran33@kenai.cs.byu.edu>
2022-10-07 00:15:33 -0600

    hw

--- hw-signals/killer.c
+++ hw-signals/killer.c
@@ -31,22 +31,104 @@ int main(int argc, char *argv[]) {
 		kill(pid, SIGHUP);
 		break;
 	case '1':
-		break;
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 	case '2':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '3':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '4':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGINT);
+		sleep(1);
+
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '5':
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '6':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid, 16);
+		sleep(1);
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '7':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '8':
+		kill(pid, SIGHUP);
+		sleep(1);
+		kill(pid,31);
+		sleep(1);
+		kill(pid, 10);
+		sleep(1);
+		kill(pid,30);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
 		break;
 	case '9': 
+		kill(pid,31);
+		sleep(1);
+		kill(pid, SIGQUIT);
+		sleep(1);
+		kill(pid,31);
+		sleep(1);
+		kill(pid,31);
+		sleep(1);
+
+		
+
+		kill(pid,12);
+		sleep(1);
+		kill(pid, SIGTERM);
+		sleep(1);
+
 		break;
 
 	}
